13b29ff14f2e0995cd60375aaff6fa8f24d7638d
Vaibhav Jha <vaibhavjha22@gmail.com>
2024-08-18 09:37:39 +0530

    created product page

---
+++ src/jsondb/products.ts
@@ -0,0 +1,62 @@
+export const demoProducts = [
+    {
+      id: "1",
+      name: "Playstation 5",
+      image: "https://m.media-amazon.com/images/I/51afJC92cgL._SL1000_.jpg",
+      price: 44690,
+    },
+    {
+      id: "2",
+      name: "Xbox Series X",
+      image: "https://m.media-amazon.com/images/I/61-jjE67uqL._SL1500_.jpg",
+      price: 45990,
+    },
+    {
+      id: "3",
+      name: "Nintendo Switch",
+      image: "https://m.media-amazon.com/images/I/71Q54HnKxwS._SL1500_.jpg",
+      price: 28100,
+    },
+    {
+      id: "4",
+      name: "Oculus Quest 2",
+      image: "https://m.media-amazon.com/images/I/61GhF+JUXGL._SL1500_.jpg",
+      price: 28799,
+    },
+    {
+      id: "5",
+      name: "ASUS ROG Ally",
+      image: "https://m.media-amazon.com/images/I/71MUfCIyAWL._SL1500_.jpg",
+      price: 48990,
+    },
+    {
+      id: "6",
+      name: "Steam Deck",
+      image: "https://m.media-amazon.com/images/I/51Nw-kEg9zL._SL1100_.jpg",
+      price: 43999,
+    },
+    {
+      id: "7",
+      name: "Playstation VR2",
+      image: "https://m.media-amazon.com/images/I/61wNsoxIa5L._SL1500_.jpg",
+      price: 61999,
+    },
+    {
+      id: "8",
+      name: "SUP Handheld",
+      image: "https://m.media-amazon.com/images/I/513-6N3o0BL.jpg",
+      price: 649,
+    },
+    {
+      id: "9",
+      name: "GSH Gamestick",
+      image: "https://m.media-amazon.com/images/I/41C87gkh2jL.jpg",
+      price: 3399,
+    },
+    {
+      id: "10",
+      name: "JioDive VR",
+      image: "https://m.media-amazon.com/images/I/41S6uEKBSOL._SL1000_.jpg",
+      price: 1399,
+    },
+  ]
